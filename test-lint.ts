51be973bbc7d14f1472bb5bf5dcb32ea21a00282
parth-sarawgi09 <parth.sarawgi@kalvium.community>
2026-01-23 15:53:19 +0530

    chore: Remove test lint file

--- test-lint.ts
+++
@@ -1,6 +0,0 @@
-// Test file to demonstrate linting and pre-commit hooks
-export const testFunction = () => {
-  const unused_var = 5;
-  console.log("This should trigger a warning");
-  return "test";
-};
